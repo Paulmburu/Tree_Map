5c697b6b235d22f98209144066a0c8e9cad7521a
Paulmburu <paulmburu53@gmail.com>
2020-04-23 12:19:56 +0300

    setup bottomNav with navcontroller

--- app/src/main/java/tk/paulmburu/treemap/MainActivity.kt
+++ app/src/main/java/tk/paulmburu/treemap/MainActivity.kt
@@ -2,6 +2,7 @@ package tk.paulmburu.treemap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
@@ -9,12 +10,15 @@ import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import tk.paulmburu.treemap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var bNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,17 +29,13 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.myNavHostFragment)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout  )
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        findViewById<NavigationView>(R.id.navView)
+        findViewById<BottomNavigationView>(R.id.bottom_navigation)
             .setupWithNavController(navController)
 
-//        val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
-//
-//
-//        setSupportActionBar(findViewById(R.id.my_toolbar))
-//
-//        findViewById<Toolbar>(R.id.my_toolbar)
-//            .setupWithNavController(navController, appBarConfiguration)
+
+
+
     }
 }
