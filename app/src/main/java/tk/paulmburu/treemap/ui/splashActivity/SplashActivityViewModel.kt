0553051281d719101a76b7e146dd924e16787fd9
Paulmburu <paulmburu53@gmail.com>
2020-04-24 18:32:32 +0300

    create new firestore arborist

--- app/src/main/java/tk/paulmburu/treemap/ui/splashActivity/SplashActivityViewModel.kt
+++ app/src/main/java/tk/paulmburu/treemap/ui/splashActivity/SplashActivityViewModel.kt
@@ -1,8 +1,11 @@
 package tk.paulmburu.treemap.ui.splashActivity
 
+import tk.paulmburu.treemap.repository.FirestoreRepository
 import tk.paulmburu.treemap.user.UserManager
 
 class SplashActivityViewModel(private val userManager: UserManager) {
+    var firebaseRepository = FirestoreRepository()
+
     private var username: String? = null
     private var userEmail: String? = null
     private var password: String? = null
@@ -20,6 +23,7 @@ class SplashActivityViewModel(private val userManager: UserManager) {
         assert(userEmail != null)
         assert(password != null)
 
+        firebaseRepository.createNewArborist(username!!,userEmail!!)
         userManager.registerUser(username!!, userEmail!!, password!!)
     }
 }
