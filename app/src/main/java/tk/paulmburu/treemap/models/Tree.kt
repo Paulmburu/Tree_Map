dd7b348c3e733d451258d6efd6d688f53a30d27e
Paulmburu <paulmburu53@gmail.com>
2020-04-30 20:00:52 +0300

    update region + tree species fields

--- app/src/main/java/tk/paulmburu/treemap/models/Tree.kt
+++ app/src/main/java/tk/paulmburu/treemap/models/Tree.kt
@@ -4,12 +4,14 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.firestore.GeoPoint
 
 data class Tree constructor(
-    val tree_id: String,
-    val name: String,
-    val tree_image: String,
-    val aborist_username: String,
-    val arborist_email: String,
-    val tree_geopoint: GeoPoint,
-    val timeStampString: Long,
-    val treeDescription: String
+    val timeStampString: Long = 0,
+    val tree_id: String = "",
+    val name: String = "",
+    val tree_species: String = "",
+    val region: String = "",
+    val tree_image: String = "",
+    val aborist_username: String = "",
+    val arborist_email: String = "",
+    val tree_geopoint: GeoPoint = GeoPoint(0.0,0.0),
+    val treeDescription: String = ""
 )
