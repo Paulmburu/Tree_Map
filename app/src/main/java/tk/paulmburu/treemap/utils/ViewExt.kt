b7eef35e1850a9a172db5fdb9a1212bdc2e3f71f
Paulmburu <paulmburu53@gmail.com>
2020-05-29 23:38:56 +0300

    showText extension function & snankbar function

--- app/src/main/java/tk/paulmburu/treemap/utils/ViewExt.kt
+++ app/src/main/java/tk/paulmburu/treemap/utils/ViewExt.kt
@@ -1,6 +1,9 @@
 package tk.paulmburu.treemap.utils
 
+import android.content.Context
 import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 fun View.setOnSingleClickListener(l: View.OnClickListener) {
     setOnClickListener(OnSingleClickListener(l))
@@ -8,4 +11,12 @@ fun View.setOnSingleClickListener(l: View.OnClickListener) {
 
 fun View.setOnSingleClickListener(l: (View) -> Unit) {
     setOnClickListener(OnSingleClickListener(l))
+}
+
+fun Context.showToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+}
+
+fun showSnackbar(view: View, message: String) {
+    Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
 }
