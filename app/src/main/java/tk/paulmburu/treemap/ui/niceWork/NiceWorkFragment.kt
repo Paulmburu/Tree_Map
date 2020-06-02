d3a9d6c14da8d061a119d4ee6f7df239efc3facb
Paulmburu <paulmburu53@gmail.com>
2020-06-02 23:43:50 +0300

    add NiceWork fragment to UI package

--- app/src/main/java/tk/paulmburu/treemap/ui/niceWork/NiceWorkFragment.kt
+++ app/src/main/java/tk/paulmburu/treemap/ui/niceWork/NiceWorkFragment.kt
@@ -1,10 +1,11 @@
-package tk.paulmburu.treemap
+package tk.paulmburu.treemap.ui.niceWork
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import tk.paulmburu.treemap.R
 
 
 class NiceWorkFragment : Fragment() {
