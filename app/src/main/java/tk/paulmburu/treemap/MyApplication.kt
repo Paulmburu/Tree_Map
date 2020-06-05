f21d9aaa8f64830378384495a42ba0564d031993
Paulmburu <paulmburu53@gmail.com>
2020-06-05 23:50:13 +0300

    init Shared Prefs when Application starts

--- app/src/main/java/tk/paulmburu/treemap/MyApplication.kt
+++ app/src/main/java/tk/paulmburu/treemap/MyApplication.kt
@@ -1,11 +1,27 @@
 package tk.paulmburu.treemap
 
 import android.app.Application
+import androidx.preference.PreferenceManager
 import tk.paulmburu.treemap.storage.SharedPreferencesStorage
 import tk.paulmburu.treemap.user.UserManager
+import tk.paulmburu.treemap.utils.ThemeHelper
 
 class MyApplication : Application() {
     val userManager by lazy {
         UserManager(SharedPreferencesStorage(this))
+    }
+
+
+    override fun onCreate() {
+        super.onCreate()
+        initThemeSharedPrefs()
+    }
+
+    fun initThemeSharedPrefs(){
+        val sharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(this)
+        val themePref =
+            sharedPreferences.getString("themePref", ThemeHelper.DEFAULT_MODE)
+        ThemeHelper.applyTheme(themePref!!)
     }
 }
