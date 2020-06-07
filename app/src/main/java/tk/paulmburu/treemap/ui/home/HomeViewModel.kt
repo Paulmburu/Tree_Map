782100876c29bb2c60d5a25dea07cb0178276746
Paulmburu <paulmburu53@gmail.com>
2020-06-07 23:50:00 +0300

    asynchronous data streams using Flows

--- app/src/main/java/tk/paulmburu/treemap/ui/home/HomeViewModel.kt
+++ app/src/main/java/tk/paulmburu/treemap/ui/home/HomeViewModel.kt
@@ -4,62 +4,78 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.MetadataChanges
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
 import tk.paulmburu.treemap.models.Arborist
 import tk.paulmburu.treemap.models.Tree
 import tk.paulmburu.treemap.repository.FirestoreRepository
 
-class HomeViewModel(): ViewModel() {
+class HomeViewModel() : ViewModel() {
 
     private val TAG = "BUBA_HOME"
     val firebaseRepository = FirestoreRepository()
 
-    private val _treesList = MutableLiveData<List<Tree>>()
-    val treesList: LiveData<List<Tree>>
-        get() = _treesList
+    private var arboristMap = HashMap<String, Arborist>()
+
+    private val _arboristList = MutableLiveData<List<Arborist>>()
+    val arboristList: LiveData<List<Arborist>>
+        get() = _arboristList
 
 
     private val _loadingDataState = MutableLiveData<LoadingState>()
-    val loadingDataState : LiveData<LoadingState>
+    val loadingDataState: LiveData<LoadingState>
         get() = _loadingDataState
 
-    companion object {
-        var list = arrayListOf<Tree>()
-        var arboristList = arrayListOf<Arborist>()
+
+    init {
+
+        viewModelScope.launch {
+            myFlowData().collect {
+                Log.d(TAG, "My Flow data -> ${it}")
+                arboristMap.put(
+                    it.aborist_username,
+                    Arborist(
+                        it.aborist_username,
+                        it.arborist_email,
+                        "Planted " + it.tree_id + " trees",
+                        "150"
+                    )
+                )
+
+                _arboristList.value = arboristMap.values.toList()
+            }
+        }
     }
 
-    fun readData(firebaseCallback: tk.paulmburu.treemap.ui.home.FirebaseCallback) {
-        _loadingDataState.value = Loading("loading data")
+    fun myFlowData(): Flow<Tree> = callbackFlow {
 
-        firebaseRepository.getAllTrees()
+        val subscription = firebaseRepository.getAllTrees()
             .addSnapshotListener(MetadataChanges.INCLUDE) { querySnapshot, e ->
                 if (e != null) {
                     Log.w(TAG, "Listen error", e)
                     _loadingDataState.value = LoadingError("ERROR !")
                     return@addSnapshotListener
                 }
-//                querySnapshot.documentChanges.forEach { documentChange -> list.add(documentChange.document.toObject()) }
-
                 for (change in querySnapshot!!.documentChanges) {
                     if (change.type == DocumentChange.Type.ADDED) {
                         Log.d(TAG, "New city: ${change.document.data}")
-                        HomeFragment.list.add(change.document.toObject(Tree::class.java))
+                        offer(change.document.toObject(Tree::class.java))
                     }
                     val source = if (querySnapshot.metadata.isFromCache)
                         "local cache"
                     else
                         "server"
-//                    Log.d("$TAG", "Data fetched from $source")
+                    Log.d("$TAG", "Data fetched from $source")
                 }
-//                Log.d("$TAG ->list", "${HomeFragment.list}")
-                firebaseCallback.onCallback(HomeFragment.list)
-
             }
-        _loadingDataState.value = LoadingDone("Done !")
+        //Finally if collect is not in use or collecting any data we cancel this channel to prevent
+        // any leak and remove the subscription listener to the database
+        awaitClose { subscription.remove() }
     }
-}
-
-interface FirebaseCallback {
-    fun onCallback(list: List<Tree>)
 }
